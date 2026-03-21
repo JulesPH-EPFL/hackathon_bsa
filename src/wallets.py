cb49aaaca41e132470a4e50b54fe19ce1cf5247d
JulesPH-EPFL <160861816+JulesPH-EPFL@users.noreply.github.com>
2026-03-21 17:35:35 +0100

    wallets.py

--- src/wallets.py
+++ src/wallets.py
@@ -1,4 +1,6 @@
 import json
+import os
+from xrpl.wallet import Wallet
 from xrpl.wallet import generate_faucet_wallet
 from src.config import client
 
@@ -19,8 +21,18 @@ def load_wallets() -> dict:
 def add_wallet(name: str, role: str) -> dict:
     wallets = load_wallets() if os.path.exists(WALLETS_FILE) else {}
     wallet_id = f"{role}_{name.lower().replace(' ', '_')}"
+    if wallet_id in wallets:
+        print("Ce portefeuille existe déjà !")
+        return wallets[wallet_id]
     wallets[wallet_id] = create_wallet(name)
     with open(WALLETS_FILE, "w") as f:
         json.dump(wallets, f, indent=2)
     return wallets[wallet_id]
+
         
+def get_wallet(id: str) -> Wallet:
+    wallets = load_wallets() if os.path.exists(WALLETS_FILE) else {}
+    if id not in wallets:
+        raise KeyError(f"Wallet '{id}' not found in {WALLETS_FILE}")
+    wallet_data = wallets[id]
+    return Wallet.from_seed(wallet_data["seed"])
